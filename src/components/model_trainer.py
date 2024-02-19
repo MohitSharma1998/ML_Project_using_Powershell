4121b585482b2d6c47c192f7e1c861384f32e102
Mohit Sharma <mohit1d.lp@gmail.com>
2024-02-19 18:19:00 +0530

    Model trainer with hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -53,8 +53,8 @@ class Modeltrainer:
             )
 
             models = {
-                "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
+                "Random Forest": RandomForestRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
                 "K-Neighbors Regressor": KNeighborsRegressor(),
@@ -63,13 +63,59 @@ class Modeltrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
-            model_report = evaluate_models(
+            params = {
+                "Decision Tree": {
+                    "criterion": [
+                        "squared_error",
+                        "friedman_mse",
+                        "absolute_error",
+                        "poisson",
+                    ],
+                    "max_features": ["sqrt", "log2"],
+                },
+                "Random Forest": {
+                    "criterion": [
+                        "squared_error",
+                        "friedman_mse",
+                        "absolute_error",
+                        "poisson",
+                    ],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "Gradient Boosting": {
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.90],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "Linear Regression": {},
+                "K-Neighbors Regressor": {
+                    "n_neighbors": [5, 7, 9, 11],
+                    "weights": ["uniform", "distance"],
+                },
+                "XGBRegressor": {
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "CatBoost Regressor": {
+                    "depth": [6, 8, 10],
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "iterations": [30, 50, 100],
+                },
+                "AdaBoost Regressor": {
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "loss": ["linear", "square", "exponential"],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+            }
+
+            model_report: dict = evaluate_models(
                 X_train=X_train,
                 y_train=y_train,
                 X_test=X_test,
                 y_test=y_test,
-                models=models
-            )
+                models=models,
+                params=params
+            )  # Will give a dictionary with model names and r_squared values
 
             # To get best model score from model_report
             best_model_score = max(list(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -22,7 +23,7 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     """Function to fetch r2_score of models
 
     Args:
@@ -39,13 +40,16 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            parameters_for_model_i = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, parameters_for_model_i, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+
             model.fit(X_train, y_train)
 
-            # y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
-
-            # train_model_score = r2_score(y_train, y_train_pred)
 
             test_model_score = r2_score(y_test, y_test_pred)
 
